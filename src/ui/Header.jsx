40969c34f840bd630c1796f5b30a5e85bcecdaae
Yellowjersey <Bradfordgavin@gmail.com>
2023-09-09 14:42:51 -0400

    Day 1: Added React-Router routes

---
+++ src/ui/Header.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <SearchOrder />
+
+      <p>Gavin</p>
+    </header>
+  );
+}
+
+export default Header;
