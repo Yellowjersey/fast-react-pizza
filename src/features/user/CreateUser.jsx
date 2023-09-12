00959882e1e5e4864e2fee3db4bbe06b0769d05f
Yellowjersey <Bradfordgavin@gmail.com>
2023-09-12 13:01:54 -0400

    Added styles to app using TailwindCSS

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useState } from "react";
+import Button from "../../ui/Button";
 
 function CreateUser() {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -9,18 +10,21 @@ function CreateUser() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <p>👋 Welcome! Please start by telling us your name:</p>
+      <p className="md: mb-4 text-base text-sm text-stone-600">
+        👋 Welcome! Please start by telling us your name:
+      </p>
 
       <input
+        className="input mb-8 w-72 text-center font-semibold "
         type="text"
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
       />
 
-      {username !== '' && (
+      {username !== "" && (
         <div>
-          <button>Start ordering</button>
+          <Button type="primary">Start ordering</Button>
         </div>
       )}
     </form>
---
+++ src/features/user/UserName.jsx
@@ -0,0 +1,5 @@
+function UserName() {
+  return <div className="hidden text-sm font-semibold sm:block">Gavin</div>;
+}
+
+export default UserName;
